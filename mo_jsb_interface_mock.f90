2fc9c4cc30cbd301b1b37350f589e3d53d149201
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-02-22 16:15:10 +0100

    Shortened declarations and added first serialbox code

--- mo_jsb_interface_mock.f90
+++ mo_jsb_interface_mock.f90
@@ -10,6 +10,7 @@
 MODULE mo_jsb_interface_mock
 
   USE mo_kind,                ONLY: wp
+  USE m_serialize
 
   IMPLICIT NONE
   PRIVATE
@@ -21,167 +22,48 @@ MODULE mo_jsb_interface_mock
     MODULE PROCEDURE interface_inquire
   END INTERFACE jsbach_interface
 
-  CHARACTER(len=*), PARAMETER :: modname = 'mo_jsb_interface'
+  TYPE(t_serializer) :: jsbmock_serializer
+  TYPE(t_savepoint) :: jsbmock_savepoint
+  LOGICAL :: jsbmock_capture_enabled
+  LOGICAL :: jsbmock_replay_enabled
 
 CONTAINS
 
-  SUBROUTINE interface_full( &
-    & model_id,         &
-    & iblk, ics, ice, dtime, steplen, &
-    & t_air,            &
-    & q_air,            &
-    & rain,             &  
-    & snow,             &
-    & wind_air,         &
-    & wind_10m,         &
-    & lw_srf_down,      &
-    & swvis_srf_down,   &
-    & swnir_srf_down,   &
-    & swpar_srf_down,   &
-    & fract_par_diffuse, &
-    & press_srf,        &
-    & drag_srf,         &
-    & t_acoef,          &
-    & t_bcoef,          &
-    & q_acoef,          &
-    & q_bcoef,          &
-    & pch,              &
-    & cos_zenith_angle, &
-    & CO2_air,          &
-    & t_srf,            &
-    & t_eff_srf,        &
-    & qsat_srf,         &
-    & s_srf,            &
-    & fact_q_air,       &
-    & fact_qsat_srf,    &
-    & evapotrans,       &
-    & latent_hflx,      &
-    & sensible_hflx,    &
-    & grnd_hflx,        &
-    & grnd_hcap,        &
-    & rough_h_srf,      &
-    & rough_m_srf,      &
-    & q_snocpymlt,      &
-    & alb_vis_dir,      &
-    & alb_nir_dir,      &
-    & alb_vis_dif,      &
-    & alb_nir_dif,      &
-    & CO2_flux,         &
-    ! For lakes (optional):
-    & drag_wtr, drag_ice,                                               & !< in
-    & t_acoef_wtr, t_bcoef_wtr, q_acoef_wtr, q_bcoef_wtr,               & !< in
-    & t_acoef_ice, t_bcoef_ice, q_acoef_ice, q_bcoef_ice,               & !< in
-    & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, & !< out
-    & albedo_lwtr,                                                      & !< out
-    & t_lice, qsat_lice, evapo_ice, latent_hflx_ice, sensible_hflx_ice, & !< out
-    & albedo_lice,                                                      & !< out
-    & ice_fract_lake,                                                   & !< out
-    ! For standalone jsbach
-    & evapopot                                                          & !< out; optional
-    & )
+  SUBROUTINE jsbmock_start_capture(directory, prefix, savepoint, append)
+    CHARACTER(LEN=*), INTENT(IN)  :: directory, prefix, savepoint
+    LOGICAL, INTENT(in), OPTIONAL :: append
+  END SUBROUTINE jsbmock_start_capture
 
-    ! Arguments
-    INTEGER, INTENT(in) :: &
-      & model_id,          &
-      & iblk,              &
-      & ics,               &
-      & ice
+  SUBROUTINE interface_full(model_id, iblk, ics, ice, dtime, steplen, t_air, q_air, rain, snow, wind_air, wind_10m,            &
+    & lw_srf_down, swvis_srf_down, swnir_srf_down, swpar_srf_down, fract_par_diffuse, press_srf, drag_srf, t_acoef,            &
+    & t_bcoef, q_acoef, q_bcoef, pch, cos_zenith_angle, CO2_air, t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+    & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
+    & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
+    & drag_wtr, drag_ice, t_acoef_wtr, t_bcoef_wtr, q_acoef_wtr, q_bcoef_wtr, t_acoef_ice, t_bcoef_ice, q_acoef_ice,           &
+    & q_bcoef_ice, t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice,           &
+    & evapo_ice, latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, evapopot)
 
-    REAL(wp), INTENT(in) :: dtime, steplen
-    REAL(wp), INTENT(in) ::    &
-      & t_air             (:), &
-      & q_air             (:), &
-      & rain              (:), &
-      & snow              (:), &
-      & wind_air          (:), &
-      & wind_10m          (:), &
-      & lw_srf_down       (:), &
-      & swvis_srf_down    (:), &
-      & swnir_srf_down    (:), &
-      & swpar_srf_down    (:), &
-      & fract_par_diffuse  (:), &
-      & press_srf         (:), &
-      & drag_srf          (:), &
-      & t_acoef           (:), &
-      & t_bcoef           (:), &
-      & q_acoef           (:), &
-      & q_bcoef           (:), &
-      & pch               (:), &
-      & cos_zenith_angle  (:), &
-      & CO2_air           (:)
-    ! For lakes:
-    REAL(wp), INTENT(in), OPTIONAL ::    &
-      & drag_wtr          (:), &
-      & drag_ice          (:), &
-      & t_acoef_wtr       (:), &
-      & t_bcoef_wtr       (:), &
-      & q_acoef_wtr       (:), &
-      & q_bcoef_wtr       (:), &
-      & t_acoef_ice       (:), &
-      & t_bcoef_ice       (:), &
-      & q_acoef_ice       (:), &
-      & q_bcoef_ice       (:)
-    REAL(wp), INTENT(out) ::   &
-      & t_srf             (:), &
-      & t_eff_srf         (:), &
-      & qsat_srf          (:), &
-      & s_srf             (:), &
-      & fact_q_air        (:), &
-      & fact_qsat_srf     (:), &
-      & evapotrans        (:), &
-      & latent_hflx       (:), &
-      & sensible_hflx     (:), &
-      & grnd_hflx         (:), &
-      & grnd_hcap         (:), &
-      & rough_h_srf       (:), &
-      & rough_m_srf       (:), &
-      & q_snocpymlt       (:), &
-      & alb_vis_dir       (:), &
-      & alb_nir_dir       (:), &
-      & alb_vis_dif       (:), &
-      & alb_nir_dif       (:), &
-      & CO2_flux          (:)
-    ! For lakes:
-    REAL(wp), INTENT(out), OPTIONAL ::   &
-      & t_lwtr            (:), &
-      & qsat_lwtr         (:), &
-      & evapo_wtr         (:), &
-      & latent_hflx_wtr   (:), &
-      & sensible_hflx_wtr (:), &
-      & albedo_lwtr       (:), &
-      & t_lice            (:), &
-      & qsat_lice         (:), &
-      & evapo_ice         (:), &
-      & latent_hflx_ice   (:), &
-      & sensible_hflx_ice (:), &
-      & albedo_lice       (:), &
-      & ice_fract_lake    (:)
-    ! For standalone jsbach:
+    INTEGER, INTENT(in) :: model_id, iblk, ics, ice
+    REAL(wp), INTENT(in) :: dtime, steplen, t_air(:), q_air(:), rain(:), snow(:), wind_air(:), wind_10m(:), lw_srf_down(:),    &
+      & swvis_srf_down(:), swnir_srf_down(:), swpar_srf_down(:), fract_par_diffuse(:), press_srf(:), drag_srf(:), t_acoef(:),  &
+      & t_bcoef(:), q_acoef(:), q_bcoef(:), pch(:), cos_zenith_angle(:), CO2_air(:)
+    REAL(wp), INTENT(in), OPTIONAL :: drag_wtr(:), drag_ice(:), t_acoef_wtr(:), t_bcoef_wtr(:), q_acoef_wtr(:),                &
+      & q_bcoef_wtr(:), t_acoef_ice(:), t_bcoef_ice(:), q_acoef_ice(:), q_bcoef_ice(:)
+    REAL(wp), INTENT(out) :: t_srf(:), t_eff_srf(:), qsat_srf(:), s_srf(:), fact_q_air(:), fact_qsat_srf(:), evapotrans(:),    &
+      & latent_hflx(:), sensible_hflx(:), grnd_hflx(:), grnd_hcap(:), rough_h_srf(:), rough_m_srf(:), q_snocpymlt(:),          &
+      & alb_vis_dir(:), alb_nir_dir(:), alb_vis_dif(:), alb_nir_dif(:), CO2_flux(:)
+    REAL(wp), INTENT(out), OPTIONAL :: t_lwtr(:), qsat_lwtr(:), evapo_wtr(:), latent_hflx_wtr(:), sensible_hflx_wtr(:),        &
+      & albedo_lwtr(:), t_lice(:), qsat_lice(:), evapo_ice(:), latent_hflx_ice(:), sensible_hflx_ice(:), albedo_lice(:),       &
+      & ice_fract_lake(:)
     REAL(wp), INTENT(out), OPTIONAL :: evapopot(:)
 
   END SUBROUTINE interface_full
 
-  !
   ! Not used currently
-  SUBROUTINE interface_inquire( model_id, iblk, ics, ice,         &
-    & t_srf, t_eff_srf, qsat_srf, zh_srf, zm_srf, alb_vis, alb_nir)
+  SUBROUTINE interface_inquire( model_id, iblk, ics, ice, t_srf, t_eff_srf, qsat_srf, zh_srf, zm_srf, alb_vis, alb_nir)
 
-    CHARACTER(len=*), PARAMETER :: routine = modname//':interface_inquire'
-
-    INTEGER, INTENT(in) :: &
-      & model_id,          &
-      & iblk,              &
-      & ics,               &
-      & ice
-
-    REAL(wp), INTENT(inout) :: &
-      & t_srf    (:), &
-      & t_eff_srf(:), &
-      & qsat_srf (:), &
-      & zh_srf   (:), &
-      & zm_srf   (:), &
-      & alb_vis  (:), &
-      & alb_nir  (:)
+    INTEGER, INTENT(in) :: model_id, iblk, ics, ice
+    REAL(wp), INTENT(inout) :: t_srf(:), t_eff_srf(:), qsat_srf(:), zh_srf(:), zm_srf(:), alb_vis(:), alb_nir(:)
 
   END SUBROUTINE interface_inquire
 
