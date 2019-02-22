547cc16273aee5076ae4661295ac0449661b4db1
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-02-22 16:48:30 +0100

    Implemented read and write routines

--- mo_jsb_interface_mock.f90
+++ mo_jsb_interface_mock.f90
@@ -58,7 +58,7 @@ CONTAINS
     REAL(wp), INTENT(out), OPTIONAL :: evapopot(:)
 
     IF (jsbmock_replay_enabled) THEN
-      CALL jsbach_replay(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+      CALL jsbmock_read(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
         & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,    &
         & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                     &
         & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,         &
@@ -74,7 +74,7 @@ CONTAINS
         & q_bcoef_ice, t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice,       &
         & evapo_ice, latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, evapopot)
 
-      CALL jsbach_capture(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+      CALL jsbmock_write(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
         & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,    &
         & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                     &
         & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,         &
@@ -92,7 +92,7 @@ CONTAINS
 
   END SUBROUTINE interface_inquire
 
-  SUBROUTINE jsbach_capture(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+  SUBROUTINE jsbmock_write(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
     & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
     & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
     & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,             &
@@ -107,9 +107,73 @@ CONTAINS
       & ice_fract_lake(:)
     REAL(wp), INTENT(in), OPTIONAL :: evapopot(:)
 
-  END SUBROUTINE jsbach_capture
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 't_srf', t_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 't_eff_srf', t_eff_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'qsat_srf', qsat_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 's_srf', s_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'fact_q_air', fact_q_air)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'fact_qsat_srf', fact_qsat_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'evapotrans', evapotrans)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx', latent_hflx)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx', sensible_hflx)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'grnd_hflx', grnd_hflx)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'grnd_hcap', grnd_hcap)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'rough_h_srf', rough_h_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'rough_m_srf', rough_m_srf)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'q_snocpymlt', q_snocpymlt)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'alb_vis_dir', alb_vis_dir)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'alb_nir_dir', alb_nir_dir)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'alb_vis_dif', alb_vis_dif)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'alb_nir_dif', alb_nir_dif)
+    CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'CO2_flux', CO2_flux)
 
-  SUBROUTINE jsbach_replay(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+    IF (PRESENT(t_lwtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 't_lwtr', t_lwtr)
+    END IF
+    IF (PRESENT(qsat_lwtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'qsat_lwtr', qsat_lwtr)
+    END IF
+    IF (PRESENT(evapo_wtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'evapo_wtr', evapo_wtr)
+    END IF
+    IF (PRESENT(latent_hflx_wtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx_wtr', latent_hflx_wtr)
+    END IF
+    IF (PRESENT(sensible_hflx_wtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx_wtr', sensible_hflx_wtr)
+    END IF
+    IF (PRESENT(albedo_lwtr)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'albedo_lwtr', albedo_lwtr)
+    END IF
+    IF (PRESENT(t_lice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 't_lice', t_lice)
+    END IF
+    IF (PRESENT(qsat_lice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'qsat_lice', qsat_lice)
+    END IF
+    IF (PRESENT(evapo_ice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'evapo_ice', evapo_ice)
+    END IF
+    IF (PRESENT(latent_hflx_ice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx_ice', latent_hflx_ice)
+    END IF
+    IF (PRESENT(sensible_hflx_ice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx_ice', sensible_hflx_ice)
+    END IF
+    IF (PRESENT(albedo_lice)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'albedo_lice', albedo_lice)
+    END IF
+    IF (PRESENT(ice_fract_lake)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'ice_fract_lake', ice_fract_lake)
+    END IF
+
+    IF (PRESENT(evapopot)) THEN
+      CALL fs_write(jsbmock_serializer, jsbmock_savepoint, 'evapopot', evapopot)
+    END IF
+
+  END SUBROUTINE jsbmock_write
+
+  SUBROUTINE jsbmock_read(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
     & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
     & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
     & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,             &
@@ -124,7 +188,71 @@ CONTAINS
       & ice_fract_lake(:)
     REAL(wp), INTENT(out), OPTIONAL :: evapopot(:)
 
-  END SUBROUTINE jsbach_replay
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 't_srf', t_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 't_eff_srf', t_eff_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'qsat_srf', qsat_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 's_srf', s_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'fact_q_air', fact_q_air)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'fact_qsat_srf', fact_qsat_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'evapotrans', evapotrans)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx', latent_hflx)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx', sensible_hflx)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'grnd_hflx', grnd_hflx)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'grnd_hcap', grnd_hcap)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'rough_h_srf', rough_h_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'rough_m_srf', rough_m_srf)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'q_snocpymlt', q_snocpymlt)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'alb_vis_dir', alb_vis_dir)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'alb_nir_dir', alb_nir_dir)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'alb_vis_dif', alb_vis_dif)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'alb_nir_dif', alb_nir_dif)
+    CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'CO2_flux', CO2_flux)
+
+    IF (PRESENT(t_lwtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 't_lwtr', t_lwtr)
+    END IF
+    IF (PRESENT(qsat_lwtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'qsat_lwtr', qsat_lwtr)
+    END IF
+    IF (PRESENT(evapo_wtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'evapo_wtr', evapo_wtr)
+    END IF
+    IF (PRESENT(latent_hflx_wtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx_wtr', latent_hflx_wtr)
+    END IF
+    IF (PRESENT(sensible_hflx_wtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx_wtr', sensible_hflx_wtr)
+    END IF
+    IF (PRESENT(albedo_lwtr)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'albedo_lwtr', albedo_lwtr)
+    END IF
+    IF (PRESENT(t_lice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 't_lice', t_lice)
+    END IF
+    IF (PRESENT(qsat_lice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'qsat_lice', qsat_lice)
+    END IF
+    IF (PRESENT(evapo_ice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'evapo_ice', evapo_ice)
+    END IF
+    IF (PRESENT(latent_hflx_ice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'latent_hflx_ice', latent_hflx_ice)
+    END IF
+    IF (PRESENT(sensible_hflx_ice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'sensible_hflx_ice', sensible_hflx_ice)
+    END IF
+    IF (PRESENT(albedo_lice)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'albedo_lice', albedo_lice)
+    END IF
+    IF (PRESENT(ice_fract_lake)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'ice_fract_lake', ice_fract_lake)
+    END IF
+
+    IF (PRESENT(evapopot)) THEN
+      CALL fs_read(jsbmock_serializer, jsbmock_savepoint, 'evapopot', evapopot)
+    END IF
+
+  END SUBROUTINE jsbmock_read
 
   SUBROUTINE jsbmock_start_capture(directory, prefix, savepoint, append)
 
