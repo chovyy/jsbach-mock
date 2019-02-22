e5980bb16e6712873347389b9793ea777ab1e736
Christian Hovy <hovy@informatik.uni-hamburg.de>
2019-02-22 16:33:11 +0100

    Stubs for capture and replay routines

--- mo_jsb_interface_mock.f90
+++ mo_jsb_interface_mock.f90
@@ -30,24 +30,28 @@ MODULE mo_jsb_interface_mock
 
 CONTAINS
 
-  SUBROUTINE interface_full(model_id, iblk, ics, ice, dtime, steplen, t_air, q_air, rain, snow, wind_air, wind_10m,            &
-    & lw_srf_down, swvis_srf_down, swnir_srf_down, swpar_srf_down, fract_par_diffuse, press_srf, drag_srf, t_acoef,            &
-    & t_bcoef, q_acoef, q_bcoef, pch, cos_zenith_angle, CO2_air, t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+  SUBROUTINE interface_full(model_id, iblk, ics, ice, &
+    & dtime, steplen, t_air, q_air, rain, snow, wind_air, wind_10m, lw_srf_down, swvis_srf_down, swnir_srf_down,               &
+    & swpar_srf_down, fract_par_diffuse, press_srf, drag_srf, t_acoef, t_bcoef, q_acoef, q_bcoef, pch, cos_zenith_angle,       &
+    & CO2_air, &
+    & t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
     & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
     & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
     & drag_wtr, drag_ice, t_acoef_wtr, t_bcoef_wtr, q_acoef_wtr, q_bcoef_wtr, t_acoef_ice, t_bcoef_ice, q_acoef_ice,           &
-    & q_bcoef_ice, t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice,           &
-    & evapo_ice, latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, evapopot)
+    & q_bcoef_ice, &
+    & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,             &
+    & latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, &
+    & evapopot)
 
     INTEGER, INTENT(in) :: model_id, iblk, ics, ice
     REAL(wp), INTENT(in) :: dtime, steplen, t_air(:), q_air(:), rain(:), snow(:), wind_air(:), wind_10m(:), lw_srf_down(:),    &
       & swvis_srf_down(:), swnir_srf_down(:), swpar_srf_down(:), fract_par_diffuse(:), press_srf(:), drag_srf(:), t_acoef(:),  &
       & t_bcoef(:), q_acoef(:), q_bcoef(:), pch(:), cos_zenith_angle(:), CO2_air(:)
-    REAL(wp), INTENT(in), OPTIONAL :: drag_wtr(:), drag_ice(:), t_acoef_wtr(:), t_bcoef_wtr(:), q_acoef_wtr(:),                &
-      & q_bcoef_wtr(:), t_acoef_ice(:), t_bcoef_ice(:), q_acoef_ice(:), q_bcoef_ice(:)
     REAL(wp), INTENT(out) :: t_srf(:), t_eff_srf(:), qsat_srf(:), s_srf(:), fact_q_air(:), fact_qsat_srf(:), evapotrans(:),    &
       & latent_hflx(:), sensible_hflx(:), grnd_hflx(:), grnd_hcap(:), rough_h_srf(:), rough_m_srf(:), q_snocpymlt(:),          &
       & alb_vis_dir(:), alb_nir_dir(:), alb_vis_dif(:), alb_nir_dif(:), CO2_flux(:)
+    REAL(wp), INTENT(in), OPTIONAL :: drag_wtr(:), drag_ice(:), t_acoef_wtr(:), t_bcoef_wtr(:), q_acoef_wtr(:),                &
+      & q_bcoef_wtr(:), t_acoef_ice(:), t_bcoef_ice(:), q_acoef_ice(:), q_bcoef_ice(:)
     REAL(wp), INTENT(out), OPTIONAL :: t_lwtr(:), qsat_lwtr(:), evapo_wtr(:), latent_hflx_wtr(:), sensible_hflx_wtr(:),        &
       & albedo_lwtr(:), t_lice(:), qsat_lice(:), evapo_ice(:), latent_hflx_ice(:), sensible_hflx_ice(:), albedo_lice(:),       &
       & ice_fract_lake(:)
@@ -72,6 +76,39 @@ CONTAINS
 
   END SUBROUTINE interface_inquire
 
+  SUBROUTINE jsbach_capture(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+    & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
+    & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
+    & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,             &
+    & latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, &
+    & evapopot)
+
+    REAL(wp), INTENT(in) :: t_srf(:), t_eff_srf(:), qsat_srf(:), s_srf(:), fact_q_air(:), fact_qsat_srf(:), evapotrans(:),    &
+      & latent_hflx(:), sensible_hflx(:), grnd_hflx(:), grnd_hcap(:), rough_h_srf(:), rough_m_srf(:), q_snocpymlt(:),          &
+      & alb_vis_dir(:), alb_nir_dir(:), alb_vis_dif(:), alb_nir_dif(:), CO2_flux(:)
+    REAL(wp), INTENT(in), OPTIONAL :: t_lwtr(:), qsat_lwtr(:), evapo_wtr(:), latent_hflx_wtr(:), sensible_hflx_wtr(:),        &
+      & albedo_lwtr(:), t_lice(:), qsat_lice(:), evapo_ice(:), latent_hflx_ice(:), sensible_hflx_ice(:), albedo_lice(:),       &
+      & ice_fract_lake(:)
+    REAL(wp), INTENT(in), OPTIONAL :: evapopot(:)
+
+  END SUBROUTINE jsbach_capture
+
+  SUBROUTINE jsbach_replay(t_srf, t_eff_srf, qsat_srf, s_srf, fact_q_air, fact_qsat_srf, &
+    & evapotrans, latent_hflx, sensible_hflx, grnd_hflx, grnd_hcap, rough_h_srf, rough_m_srf, q_snocpymlt, alb_vis_dir,        &
+    & alb_nir_dir, alb_vis_dif, alb_nir_dif, CO2_flux,                                                                         &
+    & t_lwtr, qsat_lwtr, evapo_wtr, latent_hflx_wtr, sensible_hflx_wtr, albedo_lwtr, t_lice, qsat_lice, evapo_ice,             &
+    & latent_hflx_ice, sensible_hflx_ice, albedo_lice, ice_fract_lake, &
+    & evapopot)
+
+    REAL(wp), INTENT(out) :: t_srf(:), t_eff_srf(:), qsat_srf(:), s_srf(:), fact_q_air(:), fact_qsat_srf(:), evapotrans(:),    &
+      & latent_hflx(:), sensible_hflx(:), grnd_hflx(:), grnd_hcap(:), rough_h_srf(:), rough_m_srf(:), q_snocpymlt(:),          &
+      & alb_vis_dir(:), alb_nir_dir(:), alb_vis_dif(:), alb_nir_dif(:), CO2_flux(:)
+    REAL(wp), INTENT(out), OPTIONAL :: t_lwtr(:), qsat_lwtr(:), evapo_wtr(:), latent_hflx_wtr(:), sensible_hflx_wtr(:),        &
+      & albedo_lwtr(:), t_lice(:), qsat_lice(:), evapo_ice(:), latent_hflx_ice(:), sensible_hflx_ice(:), albedo_lice(:),       &
+      & ice_fract_lake(:)
+    REAL(wp), INTENT(out), OPTIONAL :: evapopot(:)
+
+  END SUBROUTINE jsbach_replay
 
   SUBROUTINE jsbmock_start_capture(directory, prefix, savepoint, append)
 
